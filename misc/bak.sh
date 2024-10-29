a8770519b8b6c657d6ad7745d7bbca3730d432f4
cyh <1428899467@qq.com>
2024-10-29 11:45:19 +0800

    feat: 修改机器码获取方式

--- misc/bak.sh
+++ misc/bak.sh
@@ -1,13 +1,13 @@
 cp /ihouqi/nps-auth/nps-auth.service /etc/systemd/system/nps-auth.service
 
 sudo systemctl daemon-reload
+sudo systemctl enable nps-auth
+sudo systemctl restart nps-auth
+
+# 查看状态
+sudo systemctl status nps-auth
 
 sudo systemctl start nps-auth
 sudo systemctl stop nps-auth
-
-sudo systemctl status nps-auth
-
-sudo systemctl restart nps-auth
-
 
 ps |grep nps-quth |  grep -v grep
--- pkg/cert/getMachineid.go
+++ pkg/cert/getMachineid.go
@@ -1,9 +1,12 @@
 package cert
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"net"
+	"strings"
 	"sync"
-
-	"github.com/denisbrodbeck/machineid"
 )
 
 var (
@@ -13,11 +16,39 @@ var (
 
 func GetMachineID() string {
 	once.Do(func() {
-		id, err := machineid.ProtectedID("nps-auth")
-		if err != nil {
-			panic(err)
-		}
-		MachineID = id
+		MachineID = genMachineId()
 	})
 	return MachineID
+}
+
+func genMachineId() string {
+	// 获取所有网卡信息
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		panic(fmt.Sprintf("无法获取网卡信息: %v", err))
+	}
+
+	var machineCode []string
+
+	for _, iface := range interfaces {
+		// 获取网卡的 MAC 地址
+		mac := iface.HardwareAddr
+		if mac != nil {
+			machineCode = append(machineCode, mac.String())
+		}
+	}
+
+	// 组合机器码
+	result := strings.Join(machineCode, "-")
+
+	// 计算 SHA-256 哈希
+	hash := sha256.New()
+	hash.Write([]byte(result))
+	hashedValue := hash.Sum(nil)
+
+	// 将哈希值转为十六进制字符串
+	hashString := hex.EncodeToString(hashedValue)
+
+	return hashString
+
 }
